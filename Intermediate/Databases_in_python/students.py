036661494d2df04aa33b68f53a2033529cf7dc0c
Oskar <oskar.case@gmail.com>
2019-09-17 19:21:27 +0800

    New databases in python

---
+++ Intermediate/Databases_in_python/students.py
@@ -0,0 +1,41 @@
+from peewee import *
+
+db = SqliteDatabase('students.db')
+
+class Student(Model):
+    username = CharField(max_length=255, unique=True)
+    points = IntegerField(default=0)
+
+    class Meta:
+        database = db
+
+    
+    
+students = [
+    {'username' : 'oskar',
+     'points' : 4888},
+    {'username' : 'bb4',
+     'points' : 467},
+    {'username' : 'tori',
+     'points' : 48432},
+    {'username' : 'pelok',
+     'points' : 1000},
+    ]   
+
+def add_students():
+    for student in students:
+        try:
+            Student.create(username=student['username'],
+                      points=student['points'])
+        except IntegrityError:
+            student_record = Student.get(username=student['username'])
+            student_record.points = student['points']
+            student_record.save()
+            
+        
+
+
+if __name__ == '__main__':
+    db.connect()
+    db.create_tables([Student], safe=True)
+    add_students()
