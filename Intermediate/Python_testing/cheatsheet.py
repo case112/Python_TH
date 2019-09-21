ae6002b9f58e59f155ceac6189ac2fb3786bb390
Oskar <oskar.case@gmail.com>
2019-09-21 09:40:05 +0800

    Uploaded base files for python testing

---
+++ Intermediate/Python_testing/cheatsheet.py
@@ -0,0 +1,13 @@
+#doctest - A test written in a docstring.
+#doctest library - The built-in Python library for running doctests.
+
+#Running doctests
+
+#From the command line
+#python -m doctest your_script.py
+
+#From inside a script
+#import doctest
+#doctest.testmod()
+
+
