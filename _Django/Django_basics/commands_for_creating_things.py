9e33810307a9f96a0e6ffecdab8fffd0bcf92891
Oskar <oskar.case@gmail.com>
2019-09-24 21:02:44 +0800

    Some modifications and created new app courses

---
+++ _Django/Django_basics/commands_for_creating_things.py
@@ -0,0 +1,21 @@
+### Some guidance for getting a start with django project
+
+#Using vitrual enviroments
+
+    deactivate — Exit out of the current Python virtual environment
+    workon — List available virtual environments
+    workon name_of_environment — Activate the specified Python virtual environment
+    rmvirtualenv name_of_environment — Remove the specified environment.
+
+#Run server
+python3 manage.py runserver 
+
+#create new app
+$ python3 manage.py startapp courses
+
+#database migration
+python3 manage.py   migrate
+
+
+
+
--- _Django/Django_basics/learning_site/learning_site/settings.py
+++ _Django/Django_basics/learning_site/learning_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -87,7 +89,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Tallinn'
 
 USE_I18N = True
 
