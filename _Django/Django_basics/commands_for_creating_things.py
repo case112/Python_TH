f315a10d86fbb007822955adf8d196bec0b10d0e
Oskar <oskar.case@gmail.com>
2019-09-25 21:03:42 +0800

    Created a model and 2 entrys

--- _Django/Django_basics/commands_for_creating_things.py
+++ _Django/Django_basics/commands_for_creating_things.py
@@ -16,6 +16,23 @@ $ python3 manage.py startapp courses
 #database migration
 python3 manage.py   migrate
 
+jango.db.models 
+#has most of the model functionality you'll use to create models and their fields.
 
+DateTimeField #holds datetime objects.
+CharField #holds strings.
+TextField #holds an unspecified amount of text.
+
+#More Django model field types
+python manage.py makemigrations [app] #will make the migrations for a specific app.
+python manage.py migrate [app] #will run the pending migrations for a specific app. If you leave off the app name, any pending migrations for any apps will be run.
+
+python manage.py shell # opens a Python shell with Django's configuration already loaded.
+
+Model.save() #will save an in-memory instance of a model to the database.
+
+Model.create() #will save an in-memory instance of a model to the database and return the newly-created object.
+
+Model.filter(attribute=value) # will get a QuerySet of all instances of the Model that match the attribute values. You can change these values with other comparisons, too, like gte or in.
 
 
