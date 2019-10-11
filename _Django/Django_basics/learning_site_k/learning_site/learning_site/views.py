6ca962ec76a2c07fa2cb1d717bc7339c09f214b4
Oskar <oskar.case@gmail.com>
2019-10-11 18:58:56 +0800

    Bug with not getting a GET request to console

--- _Django/Django_basics/learning_site_k/learning_site/learning_site/views.py
+++ _Django/Django_basics/learning_site_k/learning_site/learning_site/views.py
@@ -8,4 +8,9 @@ def hello_world(request):
 
 def suggestion_view(request):
 	form = forms.SuggestionForm()
+	if request.method == 'POST':
+		form = forms.SuggestionForm(request.POST) #request.POST acts kinda like a dictionary
+		print('good form1')
+		if form.is_valid():
+			print('good form2')
 	return render(request, 'suggestion_form.html', {'form': form} )
