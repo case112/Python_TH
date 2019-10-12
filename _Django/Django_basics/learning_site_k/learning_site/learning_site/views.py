b2ede1c02032c12e748c64d728a3f3199c29f2f4
Oskar <oskar.case@gmail.com>
2019-10-12 16:15:17 +0800

    Solved the bug, created hidden honeypot field, created custom field validation, created email validator

--- _Django/Django_basics/learning_site_k/learning_site/learning_site/views.py
+++ _Django/Django_basics/learning_site_k/learning_site/learning_site/views.py
@@ -1,3 +1,10 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+
 from django.shortcuts import render
 
 from . import forms
@@ -10,7 +17,14 @@ def suggestion_view(request):
 	form = forms.SuggestionForm()
 	if request.method == 'POST':
 		form = forms.SuggestionForm(request.POST) #request.POST acts kinda like a dictionary
-		print('good form1')
 		if form.is_valid():
-			print('good form2')
+			send_mail(
+				'Suggestion form {}'.format(form.cleaned_data['name']),
+				form.cleaned_data['suggestion'],
+				'{name} <{email}>'.format(**form.cleaned_data),
+				['learning_site@email.com']
+			)
+			messages.add_message(request, messages.SUCCESS,
+								'Thanks for your suggestion!')
+			return HttpResponseRedirect(reverse('suggestion'))
 	return render(request, 'suggestion_form.html', {'form': form} )
