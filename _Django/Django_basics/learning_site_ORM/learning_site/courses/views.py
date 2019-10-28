9767909c117a9d295f09c7e1aca447ccfbfe47dc
Oskar <oskar.case@gmail.com>
2019-10-28 20:37:46 +0800

    Optimizing more querys, bug still there

--- _Django/Django_basics/learning_site_ORM/learning_site/courses/views.py
+++ _Django/Django_basics/learning_site_ORM/learning_site/courses/views.py
@@ -25,7 +25,8 @@ def course_list(request):
 
 def course_detail(request, pk):
     try:
-        course = models.Course.objects.prefetch_related('quiz_set', 'text_set'
+        course = models.Course.objects.prefetch_related(
+            'quiz_set', 'text_set', 'quiz_set__question_set'
             ).get(pk=pk, published=True)
     steps = sorted(chain(course.text_set.all(), course.quiz_set.all()),
                    key=lambda step: step.order)
@@ -47,8 +48,18 @@ def text_detail(request, course_pk, step_pk):
  
 
 def quiz_detail(request, course_pk, step_pk):
-    step = get_object_or_404(models.Quiz, course_id=course_pk, pk=step_pk, course__published=True)
-    return render(request, 'courses/quiz_detail.html', {'step': step})
+    try:
+        step = models.Quiz.objects.select_related(
+            'course'
+            ).prefetch_related(
+            'question_set',
+            'question_set__answer_set')
+            .get(
+            course_id=course_pk, pk=step_pk, course__published=True)
+    except models.Quiz.DoesNotExist:
+        raise Http404
+    else:
+        return render(request, 'courses/quiz_detail.html', {'step': step})
 
 
 @login_required
