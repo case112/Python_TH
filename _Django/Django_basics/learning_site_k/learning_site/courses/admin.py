894bd35c9972e65319dc32d652e2d8c34a72007c
Oskar <oskar.case@gmail.com>
2019-10-31 18:58:46 +0800

    Customizing Quizzes order

--- _Django/Django_basics/learning_site_k/learning_site/courses/admin.py
+++ _Django/Django_basics/learning_site_k/learning_site/courses/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from . import models
 
+class QuizAdmin(admin.ModelAdmin):
+	fields = ['course', 'title', 'description', 'order', 'total_questions']
+
 
 admin.site.register(models.Course)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz)
+admin.site.register(models.Quiz, QuizAdmin)
 admin.site.register(models.MultipleChoiceQuestion)
 admin.site.register(models.TrueFalseQuestion)
 admin.site.register(models.Answer)
