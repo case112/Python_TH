1eb338f07f0ea34b95d9eeeb09778bdbff0bddf1
Oskar <oskar.case@gmail.com>
2019-11-01 18:57:13 +0800

    New custom admin filters for Courses

--- _Django/Django_basics/learning_site_k/learning_site/courses/admin.py
+++ _Django/Django_basics/learning_site_k/learning_site/courses/admin.py
@@ -1,14 +1,65 @@
 from django.contrib import admin
 
+from datetime import date
+
 from . import models
 
+
+class TextInline(admin.StackedInline):
+    model = models.Text
+
+
+class QuizInline(admin.StackedInline):
+    model = models.Quiz
+
+class YearListFilter(admin.SimpleListFilter):
+	title = 'year created'
+
+	parameter_name = 'year'
+
+	def lookups(self, request, model_admin):
+		return(
+			('2015', '2015'),
+			('2016', '2016'),
+			)
+
+	def queryset(self, request, queryset):
+		if self.value() == '2015':
+			return queryset.filter(created_at__gte=date(2015, 1, 1),
+								created_at__lte=date(2015, 12, 31))
+		if self.value() == '2016':
+			return queryset.filter(created_at__gte=date(2016, 1, 1),
+								created_at__lte=date(2016, 12, 31))
+
+class AnswerInline(admin.StackedInline):
+    model = models.Answer
+
+    
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInline, QuizInline]
+
+    search_fields = ['title', 'description']
+
+    list_filter = ['created_at', YearListFilter]
+    
+    
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline,]
+
+    search_fields = ['promt']
+    
+    
 class QuizAdmin(admin.ModelAdmin):
-	fields = ['course', 'title', 'description', 'order', 'total_questions']
+    fields = ['course', 'title', 'description', 'order', 'total_questions']
+    
+    
+class TextAdmin(admin.ModelAdmin):
+    fields = ['course', 'title', 'order', 'description']
 
 
-admin.site.register(models.Course)
-admin.site.register(models.Text)
+admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.Text, TextAdmin)
 admin.site.register(models.Quiz, QuizAdmin)
-admin.site.register(models.MultipleChoiceQuestion)
-admin.site.register(models.TrueFalseQuestion)
+admin.site.register(models.MultipleChoiceQuestion, QuestionAdmin)
+admin.site.register(models.TrueFalseQuestion, QuestionAdmin)
 admin.site.register(models.Answer)
