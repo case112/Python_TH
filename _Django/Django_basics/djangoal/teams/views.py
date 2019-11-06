9aba9842a71d3891cf982873191cf9b59ffb7bee
Oskar <oskar.case@gmail.com>
2019-11-07 02:32:35 +0800

    Created ListView, DetailView and CRUD Views

--- _Django/Django_basics/djangoal/teams/views.py
+++ _Django/Django_basics/djangoal/teams/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 from . import models
 
@@ -11,3 +13,25 @@ def team_list(request):
 def team_detail(request, pk):
     team = get_object_or_404(models.Team, pk=pk)
     return render(request, 'teams/team_detail.html', {'team': team})
+
+
+class TeamListView(ListView):
+	context_object_name = 'teams'
+	model = models.Team
+
+class TeamDetailView(DetailView):
+	model = models.Team
+
+class TeamCreateView(CreateView):
+	fields = ('name', 'practice_location', 'coach')
+	model = models.Team
+
+
+class TeamUpdateView(UpdateView):
+	fields = ('name', 'practice_location', 'coach')
+	model = models.Team
+
+class TeamDeleteView(DeleteView):
+	model = models.Team
+	success_url = reverse_lazy("teams:list")
+
