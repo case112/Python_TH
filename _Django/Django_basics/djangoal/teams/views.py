4088ac4273dbc5da94dc95d545e0fee1554b5fef
Oskar <oskar.case@gmail.com>
2019-11-09 12:26:39 +0800

    Combined ListView and CreateView classes

--- _Django/Django_basics/djangoal/teams/views.py
+++ _Django/Django_basics/djangoal/teams/views.py
@@ -15,12 +15,16 @@ def team_detail(request, pk):
     return render(request, 'teams/team_detail.html', {'team': team})
 
 
-class TeamListView(ListView):
+class TeamListView(CreateView, ListView):
 	context_object_name = 'teams'
+	fields = ('name', 'practice_location', 'coach')
 	model = models.Team
+	template_name = 'teams/team_list.html'
 
-class TeamDetailView(DetailView):
+class TeamDetailView(DetailView, UpdateView):
+	fields = ('name', 'practice_location', 'coach')
 	model = models.Team
+	template_name = 'teams/team_detail.html'
 
 class TeamCreateView(CreateView):
 	fields = ('name', 'practice_location', 'coach')
