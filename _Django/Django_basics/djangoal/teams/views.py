c07fa17e532baf147ce104c9381a349ce9de6b48
Oskar <oskar.case@gmail.com>
2019-11-10 16:36:25 +0800

    Created a new mixin

--- _Django/Django_basics/djangoal/teams/views.py
+++ _Django/Django_basics/djangoal/teams/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
 from . import models
+from . import mixins
 
 
 def team_list(request):
@@ -26,9 +28,10 @@ class TeamDetailView(DetailView, UpdateView):
 	model = models.Team
 	template_name = 'teams/team_detail.html'
 
-class TeamCreateView(CreateView):
+class TeamCreateView(LoginRequiredMixin, mixins.PageTitleMixin, CreateView):
 	fields = ('name', 'practice_location', 'coach')
 	model = models.Team
+	page_title = 'Create a new team'
 
 	def get_initial(self):
 		initial = super().get_initial()
@@ -36,11 +39,17 @@ class TeamCreateView(CreateView):
 		return initial
 
 
-class TeamUpdateView(UpdateView):
+class TeamUpdateView(LoginRequiredMixin, mixins.PageTitleMixin, UpdateView):
 	fields = ('name', 'practice_location', 'coach')
 	model = models.Team
 
-class TeamDeleteView(DeleteView):
+	def get_page_title(self):
+		obj = self.get_object()
+		return "Update {}".format(obj.name)
+
+
+
+class TeamDeleteView(LoginRequiredMixin, DeleteView):
 	model = models.Team
 	success_url = reverse_lazy("teams:list")
 
