ae57aa9e93dee0ffffac33e5ee7c6dc739ed06d0
Oskar <oskar.case@gmail.com>
2019-11-12 13:14:49 +0800

    POSTing to an API view

--- _Django/Django_basics/ed_reviews/courses/urls.py
+++ _Django/Django_basics/ed_reviews/courses/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ListCourse.as_view(), name='course_list'),
+    url(r'^$', views.ListCreateCourse.as_view(), name='course_list'),
     
 ]
--- _Django/Django_basics/ed_reviews/courses/views.py
+++ _Django/Django_basics/ed_reviews/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -7,9 +8,14 @@ from . import models
 from . import serializers
 
 
-class ListCourse(APIView):
+class ListCreateCourse(APIView):
 	def get(self, request, format=None):
 		courses = models.Course.objects.all()
 		serializer = serializers.CourseSerializer(courses, many=True)
 		return Response(serializer.data)
 
+	def post(self, request, format=None):
+		serializer = serializers.CourseSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.save()
+		return Response(serializer.data, status=status.HTTP_201_CREATED)
