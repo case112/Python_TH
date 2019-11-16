e46585790c163085b4a22433ae50c49142293967
Oskar <oskar.case@gmail.com>
2019-11-17 00:56:46 +0800

    Setting throttling, customizing validation, customizing serialization

--- _Django/Django_basics/ed_reviews/courses/serializers.py
+++ _Django/Django_basics/ed_reviews/courses/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from . import models
 
@@ -18,16 +19,39 @@ class ReviewSerializer(serializers.ModelSerializer):
 		)
 		model = models.Review
 
+	def validate_rating(self, value):
+		if value in range(1, 6):
+			return value
+		raise serializers.ValidationError(
+			'Ratind must be an integer between 1 and 5')
+
+
 class CourseSerializer(serializers.ModelSerializer):
 	reviews = serializers.HyperlinkedRelatedField(
 		many=True,
 		read_only=True,
 		view_name='apiv2:review-detail')
+
+	average_rating = serializers.SerializerMethodField()
+
+
 	class Meta:
 		fields = (
 			'id',
 			'title',
 			'url',
-			'reviews'
+			'reviews',
+			'average_rating'
 		)
 		model = models.Course
+
+	def get_average_rating(self, obj):
+		average = obj.reviews.aggregate(Avg('rating')).get('rating__avg')
+
+		if average is None:
+			return 0
+
+		return round(average*2) / 2
+
+
+
