d4cc2e0aaf0a97e03281de03e7a93b62379daf70
Oskar <oskar.case@gmail.com>
2019-11-20 12:45:10 +0800

    Custom user manager

--- _Django/Django_basics/msg/accounts/models.py
+++ _Django/Django_basics/msg/accounts/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import(
+	AbstractBaseUser,
+	BaseUserManager,
+	PermissionsMixin
+)
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+	def create_user(self, email, username, display_name=None, password=None):
+		if not Email:
+			raise ValueError("Users must have an email address")
+		if not display_name:
+			display_name = username
+
+		user = self.model(
+			email=self.normalize_email(email),
+			username=username,
+			display_name=display_name
+		)
+
+		user.set_password(password)
+		user.save()
+		return user
+
+	def create_superuser(self, email, username, display_name, password):
+		user = self.create_user(
+			email,
+			username,
+			display_name,
+			password
+		)
+		user.is_staff = True
+		user.is_superuser = True
+		user.save()
+		return user
+
+
+
